e53f0443c4d3c2b0553175de4f842ad8475f0f74
Alexsey Nesterov <nesterov.alexsey@gmail.com>
2023-03-16 16:43:01 +0100

    add guardian condition to the Pizza. Finish task 1

--- src/IncorrectWeightException.java
+++ src/IncorrectWeightException.java
@@ -1,13 +1,12 @@
 //    Создайте своё собственное исключение IncorrectWeightException
 //    и унаследуйте его от IllegalArgumentException.
-public class Exception extends IllegalArgumentException {
+public class IncorrectWeightException extends IllegalArgumentException {
 
 //    Добавьте ему конструктор, в который можно передавать одно целое число
 //    При этом супер-класс IllegalArgumentException должен создаваться с сообщением об ошибке
 //    вида "Вес не может быть отрицательным: [некоректный вес]"
-  public Exception(int num) {
+  public IncorrectWeightException(int num) {
     super("Вес не может быть отрицательным: [" + num + "]");
   }
-
 }
 
