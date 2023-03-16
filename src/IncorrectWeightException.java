8d83ac27202052295cae66671caf2bab724270ff
Alexsey Nesterov <nesterov.alexsey@gmail.com>
2023-03-16 16:52:16 +0100

    Initial commit to task 2

--- src/IncorrectWeightException.java
+++ src/IncorrectWeightException.java
@@ -1,10 +1,6 @@
-//    Создайте своё собственное исключение IncorrectWeightException
-//    и унаследуйте его от IllegalArgumentException.
+
 public class IncorrectWeightException extends IllegalArgumentException {
 
-//    Добавьте ему конструктор, в который можно передавать одно целое число
-//    При этом супер-класс IllegalArgumentException должен создаваться с сообщением об ошибке
-//    вида "Вес не может быть отрицательным: [некоректный вес]"
   public IncorrectWeightException(int num) {
     super("Вес не может быть отрицательным: [" + num + "]");
   }
